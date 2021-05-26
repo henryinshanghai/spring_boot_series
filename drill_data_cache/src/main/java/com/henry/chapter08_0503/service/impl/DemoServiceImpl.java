41ff2bd8aadcc804bcaadf206215ce6828853a1b
henryInSH <2291972433@qq.com>
2021-05-26 23:02:39 +0800

    the validation approach of cache annotation function and how to switch into another cache tech

--- drill_data_cache/src/main/java/com/henry/chapter08_0503/service/impl/DemoServiceImpl.java
+++ drill_data_cache/src/main/java/com/henry/chapter08_0503/service/impl/DemoServiceImpl.java
@@ -36,7 +36,7 @@ public class DemoServiceImpl implements DemoService {
 	@Cacheable(value = "people", key = "#person.id")//3 把 特定的数据(key-value) 添加到 特定的缓存中
 	public Person findOne(Person person) {
 		Person p = personRepository.findOne(person.getId()); // 这里为什么会报错？？？ 因为使用了2.5.0版本的parent 改用1.3.0.M的版本就Okay了(这会需要pom相关的改动)
-		System.out.println("为id、key为:"+p.getId()+"数据做了缓存");
+		System.out.println("为id、key为:"+p.getId()+"数据做了缓存"); // 第二次获取相同ID的数据时，此语句预期不会输出
 		return p;
 	}
 
